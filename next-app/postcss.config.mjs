6417a1398cb865941a62e82e5807b4238ca7edbd
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-11-28 14:36:59 +0330

    CSS Modules

--- next-app/postcss.config.mjs
+++ next-app/postcss.config.mjs
@@ -6,3 +6,6 @@ const config = {
 };
 
 export default config;
+
+// in this file we have two two plugins, one is tailwind, the other is autoprefixer.
+// Now for the most part, we don't have to touch this configuration file, but if you're an advanced post CSS user, you know this is where you can provide your custom configuration. So when building our application, next JS uses post CSS to transform our class names and generate unique class names that don't clash.
