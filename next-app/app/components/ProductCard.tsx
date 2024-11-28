56a51bb0c2867bc0cc7c57e5601333841feff934
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-11-28 14:45:09 +0330

    Tailwind CSS

--- next-app/app/components/ProductCard.tsx
+++ next-app/app/components/ProductCard.tsx
@@ -1,21 +1,13 @@
 import React from "react";
 import AddToCard from "./AddToCard";
-// you can import styles from module css and named whatever you want it but it's better to called styles
-import styles from "./ProductCard.module.css";
 
 const ProductCard = () => {
   return (
-    // we have intellisense for styles object
-    <div className={styles.cardContainer}>
+    // tailwind css docs: https://tailwindcss.com/docs/installation
+    <div className="p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-600">
       <AddToCard />
     </div>
   );
 };
 
 export default ProductCard;
-
-// if you inspect the class name in source code in dev tool you see it is not ProductCard , it's ProductCard_card_KLyHh and it's auto generated, so what is happening here is that next.js uses a tool called post CSS for transforming our CSS class names.
-// the post CSS config file is postcss.config.mjs. in this file we have two two plugins, one is tailwind, the other is autoprefixer.
-// Now for the most part, we don't have to touch this configuration file, but if you're an advanced post CSS user, you know this is where you can provide your custom configuration. So when building our application, next JS uses post CSS to transform our class names and generate unique class names that don't clash.
-
-// we can group the css and tsx files in folders like ProductCard file.
