b8e4dfec2a961f9df2cc95f8de7f1bae908823c8
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-11-30 13:07:22 +0330

    Creating an Object

--- next-app/app/api/users/[id]/route.tsx
+++ next-app/app/api/users/[id]/route.tsx
@@ -4,9 +4,6 @@ export function GET(
   request: NextRequest,
   { params }: { params: { id: number } }
 ) {
-  // in real world application, we fetch data from a db
-  // if not found, return 404 error
-  // else return data
   if (params.id > 10) {
     return NextResponse.json({ error: "User not found" }, { status: 404 });
   }
--- next-app/app/api/users/route.tsx
+++ next-app/app/api/users/route.tsx
@@ -1,8 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function GET(request: NextRequest) {
   return NextResponse.json([
     { id: 1, name: "Mosh" },
     { id: 2, name: "John" },
   ]);
+}
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  if (!body.name) {
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  }
+  // status 201 for object creation successful
+  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
 }
