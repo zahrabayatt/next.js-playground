f4dfd275405e75d1a31d1efd3cf7f4d28c4aa0b5
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-11-30 16:09:15 +0330

    Installing MySQL

--- next-app/app/api/users/schema.ts
+++ next-app/app/api/users/schema.ts
@@ -1,11 +1,7 @@
 import { z } from "zod";
 
-// https://zod.dev/
-
 const schema = z.object({
   name: z.string().min(3),
-  // email: z.string().email(),
-  // age: z.number()
 });
 
 export default schema;
