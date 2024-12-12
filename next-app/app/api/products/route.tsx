7d8435d3bbf3988417345cf549a21c933b362b0c
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-12-12 23:59:50 +0330

    Exercise- Storing Products in DB

--- next-app/app/api/products/route.tsx
+++ next-app/app/api/products/route.tsx
@@ -1,12 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
+import prisma from "@/prisma/client";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function GET(request: NextRequest) {
-  return NextResponse.json([
-    { id: 1, name: "Milk", price: 2.5 },
-    { id: 2, name: "Bread", price: 3.5 },
-  ]);
+export async function GET(request: NextRequest) {
+  const products = await prisma.product.findMany();
+
+  return NextResponse.json(products);
 }
 
 export async function POST(request: NextRequest) {
@@ -17,8 +17,12 @@ export async function POST(request: NextRequest) {
     return NextResponse.json(validation.error.errors, { status: 400 });
   }
 
-  return NextResponse.json(
-    { id: 1, name: body.name, price: body.price },
-    { status: 201 }
-  );
+  const newProduct = await prisma.product.create({
+    data: {
+      name: body.name,
+      price: body.price,
+    },
+  });
+
+  return NextResponse.json(newProduct, { status: 201 });
 }
