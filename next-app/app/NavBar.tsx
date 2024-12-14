719328a913a13ce1be4ee80a8576cc62db295819
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-12-14 21:54:07 +0330

    NextAuth - Configuring Google Provider

--- next-app/app/NavBar.tsx
+++ next-app/app/NavBar.tsx
@@ -3,13 +3,10 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <div className="flex bg-slate-200 p-5">
-      <Link href="/" className="mr-5">
-        Next.js
-      </Link>
-      <Link href="/users" className="mr-5">
-        Users
-      </Link>
+    <div className="flex bg-slate-200 p-5 space-x-3">
+      <Link href="/">Next.js</Link>
+      <Link href="/users">Users</Link>
+      <Link href="/api/auth/signin">Login</Link>
     </div>
   );
 };
