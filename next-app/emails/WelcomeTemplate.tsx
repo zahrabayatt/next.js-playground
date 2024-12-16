86c65d409e1f6196952036d207e95b991f743038
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-12-16 20:52:33 +0330

    React Email - Styling Emails

--- next-app/emails/WelcomeTemplate.tsx
+++ next-app/emails/WelcomeTemplate.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 import {
   Html,
   Body,
   Container,
+  Tailwind,
   Text,
   Link,
   Preview,
@@ -16,14 +17,31 @@ const WelcomeTemplate = ({ name }: Props) => {
   return (
     <Html>
       <Preview>Welcome aboard!</Preview>
-      <Body>
-        <Container>
-          <Text>Hello {name}</Text>
-          <Link href="https://codewithme.com">www.codewithme.com</Link>
-        </Container>
-      </Body>
+      <Tailwind>
+        {/* <Body style={{ background: "black" }}> */}
+        <Body style={body}>
+          <Container>
+            <Text style={heading} className="font-bold">
+              Hello {name}
+            </Text>
+            <Link href="https://codewithme.com">www.codewithme.com</Link>
+          </Container>
+        </Body>
+      </Tailwind>
     </Html>
   );
 };
 
+const body: CSSProperties = {
+  background: "#ffd",
+};
+
+const heading: CSSProperties = {
+  fontSize: "32px",
+};
+
 export default WelcomeTemplate;
+
+// There is two ways to style our email:
+// 2- CSS properties like inline style
+// 1- Tailwind css
